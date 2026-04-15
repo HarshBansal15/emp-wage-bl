6b44b5fefebb95afa95e29b8bd9f4dbb294b9ca2
bl-practice <harshbansal15december2001@gmail,com>
2026-04-15 11:37:23 +0530

    [Harsh] Add: Python script to check emp present or not

--- emp.py
+++ emp.py
@@ -1,1 +1,16 @@
 print("Welcome")
+import random
+def check_attendance():
+	print("welcome")
+	Attendance = random.choice([1,0])
+	if Attendance == 1:
+		print("present")
+	else:
+		print("Absent")
+
+if __name__ == "__main__":
+
+ check_attendance()
+ print("welcome")
+
+
