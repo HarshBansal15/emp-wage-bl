7e0344191b30e5891cc535e6848eab6f69b3b7ce
bl-practice <harshbansal15december2001@gmail,com>
2026-04-15 11:55:51 +0530

    [Harsh]Add: python code for emp wage

--- emp.py
+++ emp.py
@@ -1,16 +1,16 @@
 print("Welcome")
 import random
-def check_attendance():
+def daily_wage():
 	print("welcome")
 	Attendance = random.choice([1,0])
 	if Attendance == 1:
-		print("present")
+		print("present and wage is 160")
 	else:
-		print("Absent")
+		print("Absent wage is 0")
 
 if __name__ == "__main__":
 
- check_attendance()
+ daily_wage()
  print("welcome")
 
 
